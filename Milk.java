e09251e99c88a80027b1ce5223698e64ca8b19e7
RinaYarovaya <91146948+RinaYarovaya@users.noreply.github.com>
2021-12-07 17:57:07 +0300

    Add files via upload

---
+++ Milk.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Milk extends Food {
+
+    private String fat;
+    public Milk (String fat)
+    {
+        super("Milk");
+        this.fat = fat;
+    }
+
+    @Override
+    public void consume() {
+        System.out.println(this + " was eaten");
+    }
+
+    public String getFat() {
+        return fat;
+    }
+    public void setFat(String fat) {
+        this.fat = fat;
+    }
+
+    public String toString() {
+        return super.toString() + " fat '" + fat.toUpperCase() + "'";
+    }
+
+}
